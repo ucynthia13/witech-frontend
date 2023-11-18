3864bfe4d795f13b3064b6b5796a6c200140b72a
Ingabire Divine <divineingabire69@gmail.com>
2023-11-18 12:20:00 +0200

    added team images

--- src/assets/data/team.js
+++ src/assets/data/team.js
@@ -1,44 +1,55 @@
+import Aline from '../team/NIYOMUNGELIAline.jpeg';
+import Divine from '../team/INGABIREDivine.jpeg';
+import Cynthia from '../team/UMWALICynthia.jpeg';
+import Regis from '../team/NDIZIHIWERegis.jpeg';
+import Igor from '../team/BISIZIMUHIREIghor.jpeg';
+import Ruth from '../team/NKUBITOYIMANZIRuth.jpeg';
+import Iris from '../team/NGABOIris.jpeg';
+
 const teamNames = [
-    {
-        name: 'Divine Ingabire',
-        title: 'Frontend Developer ',
-        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. rontEnd developer.'
-    },
+
     {
         name: 'Aline Niyomungeli',
         title: 'Designer',
-        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. rontEnd developer.'
+        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. frontEnd developer.',
+        image: Aline
+        
+    }, {
+        name: 'Divine Ingabire',
+        title: 'Frontend Developer ',
+        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. frontEnd developer.',
+        image: Divine
     },
     {
         name: 'Cynthia Umwali',
         title: 'Frontend Developer ',
-        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. rontEnd developer.'
+        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. frontEnd developer.',
+        image: Cynthia
     },
     {
         name: 'Ndizihiwe Regis',
         title: 'Backend Developer',
-        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. rontEnd developer.'
+        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. frontEnd developer.',
+        image: Regis
     },
     {
         name: 'Muhire Igor',
         title: 'Frontend Developer ',
-        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. rontEnd developer.'
+        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. frontEnd developer.',
+        image: Igor
     },
     {
         name: 'Ruth Nkubitoyimanzi',
         title: 'Frontend Developer',
-        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. rontEnd developer.'
+        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. frontEnd developer.',
+        image: Ruth
     },
     {
         name: 'Ngabo Iris',
         title: 'Frontend Developer ',
-        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. rontEnd developer.'
-    },
-    {
-        name: 'Divine Ingabire',
-        title: 'Frontend Developer',
-        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. rontEnd developer.'
-    },
+        description: 'FrontEnd developer. This is your description showing your achievements. rontEnd developer. This is your description showing your achievements. frontEnd developer.',
+        image: Iris
+    }
 
 ]
     
