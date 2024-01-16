203d1d70db06ac7755eb2b5a5336b588fc0e4375
ucynthia13 <cycyu614@gmail.com>
2024-01-16 07:55:02 +0200

    ..

--- src/components/partners/Partners.jsx
+++ src/components/partners/Partners.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import nesa from '../../assets/nesa-removebg-preview.png';
 import rca2 from '../../assets/rca-removebg-preview.png';
 import rdb from '../../assets/rdb-removebg-preview.png';
+import sb from '../../assets/sbf-removebg-preview.png'
 
 function Partners() {
   return (
     <div className='container mt-'>
       <div className='row mt-'>
         <div className='col-12 d-flex justify-content-center ms-4'>
-          <div className='col-sm-6 col-md-4 col-lg-3 mb-4'>
+          <div className='col-lg-3 mb-4'>
             <img
               src={nesa}
               className='mx-4 img-fluid '
@@ -17,7 +18,7 @@ function Partners() {
               loading='lazy'
             ></img>
           </div>
-          <div className='col-sm-6 col-md-4 col-lg-3 mb-4'>
+          <div className='col-lg-3 mb-4'>
             <img
               src={rdb}
               className='mx-4 img-fluid'
@@ -26,9 +27,18 @@ function Partners() {
               loading='lazy'
             ></img>
           </div>
-          <div className='col-sm-6 col-md-4 col-lg-3 mb-4'>
+          <div className='col-lg-3 mb-4'>
             <img
               src={rca2}
+              className='mx-4 img-fluid'
+              style={{ maxWidth: '100px', maxHeight: '100px' }}
+              alt='rca'
+              loading='lazy'
+            ></img>
+          </div>
+          <div className='col-lg-3 mb-4'>
+            <img
+              src={sb}
               className='mx-4 img-fluid'
               style={{ maxWidth: '100px', maxHeight: '100px' }}
               alt='rca'
