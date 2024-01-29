5ed294cf653c7f759cea7e8aaacc357d5bd609b9
ucynthia13 <cycyu614@gmail.com>
2024-01-29 13:46:12 +0200

    fixed empty links

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import StoryCard from './StoryCard'; 
+
 import image1 from '../../assets/ramb2.jpg';
 import image2 from '../../assets/ramb1.jpg';
 import image3 from '../../assets/IMG_0501.jpg';
-import curve from '../../assets/shape.png'
-import LearnMore from '../../assets/icons/LearnMore';
-import { useNavigate } from 'react-router-dom';
 
 function Stories() {
+
   const navigate = useNavigate();
 
   const redirectToMoreBlogs = () => {
-    const newWindow = window.open('https://photos.app.goo.gl/TEiTNqB6zaBe94f4A', '_blank')
-    if(newWindow){
-      newWindow.opener = null
+    const newWindow = window.open('https://photos.app.goo.gl/TEiTNqB6zaBe94f4A', '_blank');
+    if (newWindow) {
+      newWindow.opener = null;
     }
-  }
-
+  };
 
   const initialDivs = [
     {
@@ -34,67 +35,28 @@ function Stories() {
       description: 'Leading the charge in advancing digital literacy, our foundation is at the forefront of initiatives that empower individuals to thrive in the digital age.',
     },
   ];
-  
-  
 
-  const [divs, setDivs] = useState(initialDivs);
- 
+  const [divs] = useState(initialDivs);
+
   return (
     <div className='container text-font' id='activities'>
       <div className='row'>
         <h5 className='text-center h6 mt-4 mb-3'>Stories</h5>
         <p className='h2 text-center mb-4 fw-bolder'>
-          Read <span className='heading-color'><span className='underlined'> Our Blogs</span>
-          </span>
-        
+          Read <span className='heading-color'><span className='underlined'> Our Blogs</span></span>
         </p>
 
-        <div className='col-lg-4 mt-4'>
-          <div className='card border-0 hover-effect card-height'>
-            <img src={image1} className='img-fluid card-img-top' alt='students' loading='lazy'/>
-            <div className='card-body'>
-              <h5 className='card-title text-center fw-bold my-2'>{divs[0].title}</h5>
-              <p className='text-center team-font-size'>{divs[0].description}</p>
-              <div  onClick={()=>navigate("/article")} className='d-flex justify-content-center'>
-
-                <LearnMore />
-              </div>
-            </div>
+        {divs.map((story, index) => (
+          <div key={index} className='col-lg-4 mt-4'>
+            <StoryCard story={story} />
           </div>
-        </div>
+        ))}
 
-        <div className='col-lg-4 mt-4'>
-          <div className='card border-0 hover-effect card-height'>
-            <img src={image2} className='img-fluid card-img-top' alt='students' />
-            <div className='card-body'>
-              <h5 className='card-title text-center fw-bold my-2'>{divs[1].title}</h5>
-              <p className='text-center team-font-size'>{divs[1].description}</p>
-              <div className='d-flex justify-content-center'>
-                <LearnMore />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='col-lg-4 mt-4'>
-          <div className='card border-0 hover-effect card-height'>
-            <img src={image3} className='img-fluid card-img-top' alt='students' />
-            <div className='card-body'>
-              <h5 className='card-title text-center fw-bold my-2'>{divs[2].title}</h5>
-              <p className=' text-center team-font-size'>{divs[2].description}</p>
-              <div className='d-flex justify-content-center'>
-                <LearnMore />
-              </div>
-            </div>
-          </div>
-       
-       </div>
-
-        <div className='col-lg-12 mt-4 text-end'> 
+        <div className='col-lg-12 mt-4 text-end'>
           <button className='btn btn-secondary rounded-circle button-background me-4' onClick={redirectToMoreBlogs}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-              </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </svg>
           </button>
         </div>
       </div>
